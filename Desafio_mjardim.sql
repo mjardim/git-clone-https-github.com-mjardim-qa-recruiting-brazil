40e7a1e5ef30533203ab7dd56b2e7bda182ece9f
mjardim <45541614+mjardim@users.noreply.github.com>
2018-12-02 17:15:15 -0200

    Add files via upload

---
+++ Desafio_mjardim.sql
@@ -0,0 +1,31 @@
+--Cenários para duas funcionalidades do Whatsapp 
+
+
+Estoria01 - Excluir Conversas do Whatsapp 
+
+Eu como usuário quero poder excluir as conversas do whatsapp tanto de grupos quanto conversas particulares e realizar exclusão em massa quando achar necessário. 
+
+CN0001- Exclui Conversa manualmente 
+
+CT0001-Excluir Conversa privada individualmente 
+CT0002-Excluir Conversas privadas selecionadas 
+CT0003-Excluir Conversa de grupo manualmente 
+
+CN0002- Exclui Conversas em massa 
+CT0004-Excluir conversas de grupos e privadas em massa 
+---------------------------------------------------------------------------- 
+
+Estoria02 - Enviar mídia por Whatsapp 
+Eu como usuário quero enviar mídia para varias pessoas de uma vez para facilitar meu trabalho no whatsapp 
+
+CN0003- Envia Mídia via lista de transmissão 
+
+CT0005-Criar lista de transmissão para assunto especifico 
+CT0006-Enviar mídia para lista de transmissão criada 
+
+CN0004- Envia Mídia via grupo de whatsapp 
+CT0007-Criar grupo para assunto especifico 
+CT0008-Enviar mídia para grupo especifico 
+
+
+ 
